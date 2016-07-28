6945736f0132b68753a2cd3e989787437171f56f
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-28 14:40:31 +0900

    Add hide select button and do not reload page

--- ba-restriction.js
+++ ba-restriction.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
 	$('input.qa-a-select-button').click(function(){
 		if (confirm('ベストアンサーを一度選択すると変更はできません。よろしいですか？ (なお、選択後も回答は受け付けられます) ')) {
 			var args = $(this).data('args').split(',');
-			qa_answer_click(args[0].trim(), args[1].trim(), this);
-			location.reload();
+			var ret = qa_answer_click(args[0].trim(), args[1].trim(), this);
+			hide_select_button();
+			return ret;
 		}
 		return false;
 	});
+
+	var hide_select_button = function() {
+		$('input.qa-a-select-button').hide();
+	};
 });
