aac0ff07ac0c69ddd78745df556ae1324c8eee33
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-28 14:33:57 +0900

    Add reload page

--- ba-restriction.js
+++ ba-restriction.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	$('input.qa-a-select-button').click(function(){
 		if (confirm('ベストアンサーを一度選択すると変更はできません。よろしいですか？ (なお、選択後も回答は受け付けられます) ')) {
 			var args = $(this).data('args').split(',');
-			return qa_answer_click(args[0].trim(), args[1].trim(), this);
+			qa_answer_click(args[0].trim(), args[1].trim(), this);
+			location.reload();
 		}
 		return false;
 	});
